70e89860c7a264ee88d755363691655f8bdc6b6c
jeandev84 <jeandev84@yahoo.com>
2022-08-24 01:07:22 +0300

    refactoring vacation controller api resource

--- vacation/app/Http/Controllers/Api/V1/VacationController.php
+++ vacation/app/Http/Controllers/Api/V1/VacationController.php
@@ -3,61 +3,75 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\VacationStoreRequest;
+use App\Http\Requests\VacationUpdateRequest;
+use App\Models\Vacation;
 use Illuminate\Http\Request;
 
 class VacationController extends Controller
 {
+
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
-     */
+    */
     public function index()
     {
-        //
+        // TODO implements
     }
+
+
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param VacationStoreRequest $request
      * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    */
+    public function store(VacationStoreRequest $request)
     {
-        //
+        // TODO implements
     }
+
+
+
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  Vacation $vacation
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Vacation $vacation)
     {
-        //
+        // TODO implements
     }
+
+
+
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param VacationUpdateRequest $request
+     * @param  Vacation $vacation
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(VacationUpdateRequest $request, Vacation $vacation)
     {
         //
     }
 
+
+
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param
      * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
+    */
+    public function destroy(Vacation $vacation)
     {
         //
     }
